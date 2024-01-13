cbf5d063fa9836844962e3e365e226b813046b40
trallik <trallik74@gmail.com>
2024-01-13 16:57:58 +0400

    refactor: move auth routes to a separate module

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 const router = require('express').Router();
-const { createUser, loginUser } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const errorHandler = require('../middlewares/error-handler');
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
+const authRouter = require('./auth');
 const NotFoundError = require('../exeptions/not-found-error');
 
-router.post('/signup', createUser);
-router.post('/signin', loginUser);
+router.use('/', authRouter);
 router.use(auth);
 router.use('/users', usersRouter);
 router.use('/movies', moviesRouter);
