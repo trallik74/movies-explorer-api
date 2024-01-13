d4686750200955a8d10b3529d4f9d3abc26e3692
trallik <trallik74@gmail.com>
2024-01-13 17:16:51 +0400

    feat: add logger middlewares

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,20 @@
 const router = require('express').Router();
 const auth = require('../middlewares/auth');
+const { requestLogger, errorLogger } = require('../middlewares/logger');
 const errorHandler = require('../middlewares/error-handler');
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
 const authRouter = require('./auth');
 const NotFoundError = require('../exeptions/not-found-error');
 
+router.use(requestLogger);
 router.use('/', authRouter);
 router.use(auth);
 router.use('/users', usersRouter);
 router.use('/movies', moviesRouter);
 router.use('*', (req, res, next) => next(new NotFoundError('Запрашиваемый ресурс не найден')));
 
+router.use(errorLogger);
 router.use(errorHandler);
 
 module.exports = router;
